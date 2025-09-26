14b4b7801c738976da8444c91cc56c9aaf57482b
sinequanonn <dleck288@gmail.com>
2025-09-26 21:01:24 +0900

    feat: implement price winningLotto bonusNumber and validate input
    구입금액 입력 구현
    금액이 1000원 단위인지 검증
    당첨 번호 입력 구현
    보너스 번호 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,29 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Customer customer = new Customer(new Bag());
+        LottoMachine lottoMachine = new LottoMachine();
+        LottoStore lottoStore = new LottoStore(lottoMachine);
+
+        lottoStore.enter(customer);
+
+        Lotto lotto = WinningLottoInput.getWinningLotto();
+        WinningLotto winningLotto = new WinningLotto(lotto);
+//        System.out.println("result = " + result.getWinningLotto());
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusInput = Console.readLine();
+        Integer bonus = Integer.valueOf(bonusInput);
+//        System.out.println("bonus = " + bonus);
+
+        LottoResult lottoResult = new LottoResult(winningLotto, customer.getBag().getLottos(), bonus);
+
+
+
     }
 }
---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,14 @@
+package lotto;
+
+public class WinningLotto {
+
+    private final Lotto winningLotto;
+
+    public WinningLotto(Lotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+}
