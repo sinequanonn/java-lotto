9785d84a184ee07eea5f9ff06f04468eb5ef18cc
sinequanonn <dleck288@gmail.com>
2025-09-30 22:52:05 +0900

    feat: 매직 넘버 제거 및 상수화 위한 Constant 클래스 생성
    로또 시작 번호 로또 끝 번호 로또 공 개수 매직 넘버 제거
    상수화 기법 적용한 Constant 클래스 추가

---
+++ src/main/java/lotto/Constant.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class Constant {
+    public static Integer LOTTO_START_NUMBER = 1;
+
+    public static Integer LOTTO_END_NUMBER = 45;
+
+    public static Integer LOTTO_COUNT = 6;
+}
