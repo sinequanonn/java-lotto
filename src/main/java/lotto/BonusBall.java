01f5f2176bca0c62a9f581f1955924970a428c87
sinequanonn <dleck288@gmail.com>
2025-10-01 16:04:02 +0900

    refactor: 보너스 볼 사용자로부터 입력 로직 추가
    보너스 볼 사용자로부터 입력 로직 추가

--- src/main/java/lotto/BonusBall.java
+++ src/main/java/lotto/BonusBall.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import static lotto.Constant.LOTTO_START_NUMBER;
@@ -8,13 +9,28 @@ import static lotto.Constant.LOTTO_END_NUMBER;
 public class BonusBall {
 
     public Integer makeBonusBall(Lotto winningLotto) {
-        Integer bonusBall;
-        bonusBall = Randoms.pickNumberInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER);
-
+        String input = Console.readLine();
+        Integer bonusBall = validate(input);
         if (isDuplicated(winningLotto, bonusBall)) {
             throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복입니다.");
         }
         return bonusBall;
+    }
+
+    private Integer validate(String input) {
+        try {
+            Integer bonusBall = Integer.parseInt(input);
+            validateRange(bonusBall);
+            return bonusBall;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 정수이어야 합니다.");
+        }
+    }
+
+    private void validateRange(Integer bonusBall) {
+        if (bonusBall <= 0 || bonusBall > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+        }
     }
 
     private static boolean isDuplicated(Lotto winningLotto, Integer bonusBall) {
