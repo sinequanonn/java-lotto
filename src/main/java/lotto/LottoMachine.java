bd9ebf3163d3bea55865660944e29303de435e6c
sinequanonn <dleck288@gmail.com>
2025-10-01 16:02:07 +0900

    refactor: LottoMachine 책임 수정
    sellLottoTo 메서드 책임 제거

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -8,14 +8,6 @@ import static lotto.Constant.*;
 
 public class LottoMachine {
 
-    public void sellLottoTo(Customer customer) {
-        Long count = customer.choseLottoCount();
-        for (int i = 0; i <count; i++) {
-            customer.buy(makeLotto());
-        }
-        customer.checkLotto();
-    }
-
     public Lotto makeLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_END_NUMBER, LOTTO_COUNT);
         return new Lotto(numbers);
